3616a7747809d2a9cba70afd12ad2d926f67bf2e
Martin Bradbury <martinbradbury@btinternet.com>
2024-02-18 14:38:08 +0000

    written tests, failed, written function to pass Red, Green

--- evens.py
+++ evens.py
@@ -7,7 +7,22 @@ def even_number_of_evens(numbers):
     if the numner of even numbers is even - return True
     """
 
-    return None
+    if isinstance(numbers, list):
+        if numbers == []:
+            return False
+        else:
+            evens = 0 # initialise evens as a variable and give it an initial value of 0
+
+            for n in numbers:  # write a for loop to loop over the number of evens and increment by 1 each time
+                if n % 2 == 0:  # if the remainder when divided by 2 is = to 0
+                    evens += 1 #add 1 to evens in loop
+
+            if evens:
+                return evens % 2 == 0 #return evens if when divided by 2 = 0
+            else:
+                return False
+    else:
+        raise TypeError("A list was not passed into the function")
 
 if __name__ == "__main__":
     print(even_number_of_evens(5))
--- test_evens.py
+++ test_evens.py
@@ -3,7 +3,14 @@ from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
-    pass
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
+
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)#Writing tests here to fail and then pass in function
+        self.assertEqual(even_number_of_evens([2, 4]), True)#Writing tests here to fail and then pass in function
+        self.assertEqual(even_number_of_evens([2]), False) #Writing tests here to fail and then pass in function
+        self.assertEqual(even_number_of_evens([1, 3, 5]), False) #Write a test to see if we pass in a odd number list of odd numbers
 
 
 if __name__ == "__main__":
